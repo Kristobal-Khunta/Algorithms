c0c53dd299a999d476b2355f0c92da278ee54911
mg <xynta94@gmail.com>
2022-11-25 13:30:34 +0500

    pretify file

--- leetcode/recursion/95.medium.UniqueBinarySearchTreesII.py
+++ leetcode/recursion/95.medium.UniqueBinarySearchTreesII.py
@@ -2,11 +2,16 @@
 # Given an integer n, return all the structurally unique BST's (binary search trees), which has exactly n nodes of unique values from 1 to n. Return the answer in any order.
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 class Solution:
     # https://leetcode.com/explore/learn/card/recursion-i/253/conclusion/2384/discuss/929000/Recursive-solution-long-explanation
     def generateTrees(self, n: int) -> List[TreeNode]:
