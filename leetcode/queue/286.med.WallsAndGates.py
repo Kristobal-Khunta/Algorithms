a74799b1b6d1cae56edb40afaff9afa7d7d125c6
mg <xynta94@gmail.com>
2022-12-11 20:43:56 +0500

    add better solution

--- leetcode/queue/286.med.WallsAndGates.py
+++ leetcode/queue/286.med.WallsAndGates.py
@@ -90,3 +90,29 @@ class Solution:
 
                 rooms[next_row][next_col] = cur_val + 1
                 self.queue.append((next_row, next_col))
+
+
+class Solution(object):
+    """
+    Space=> O(1)
+    Time=> O(mn)
+    """
+
+    def numIslands(self, grid):
+        islands = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == "1":
+                    islands += 1
+                    self.part_of_island(i, j, grid)
+        return islands
+
+    def part_of_island(self, i, j, grid):
+        if i < 0 or j < 0 or i == len(grid) or j == len(grid[0]) or grid[i][j] != "1":
+            return
+        else:
+            grid[i][j] = "0"
+        self.part_of_island(i, j + 1, grid)
+        self.part_of_island(i, j - 1, grid)
+        self.part_of_island(i + 1, j, grid)
+        self.part_of_island(i - 1, j, grid)
