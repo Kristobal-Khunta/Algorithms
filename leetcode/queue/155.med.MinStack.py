94de4a81a34e4b4af63819db7f682cc9c94f2218
mg <xynta94@gmail.com>
2022-12-13 16:56:28 +0500

    update with neetcode

--- leetcode/queue/155.med.MinStack.py
+++ leetcode/queue/155.med.MinStack.py
@@ -24,3 +24,24 @@ class MinStack:
 # Approach 1: Stack of Value/ Minimum Pairs
 # Approach 2: Two Stacks
 # Approach 3: Improved Two Stacks
+
+## from neetcode
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        val = min(val, self.minStack[-1] if self.minStack else val)
+        self.minStack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
