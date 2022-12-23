2dffc017a49dea32a0a824b007e282662257a2cb
mg <xynta94@gmail.com>
2022-12-23 19:04:44 +0500

    add stack dfs solution

--- leetcode/queue/200.med.NumberofIslands.py
+++ leetcode/queue/200.med.NumberofIslands.py
@@ -35,3 +35,64 @@ class Solution:
                     continue
                 queue.append((nx, ny))
                 grid[nx][ny] = "0"
+
+
+class SolutionDFS:
+    """
+    DFS
+    Time complexity : O(MxN)
+    Space complexity : worst case O(MxN)
+    """
+
+    def numIslands(self, grid: List[List[str]]) -> int:
+        self.num_islands = 0
+        self.m = len(grid)
+        self.n = len(grid[0])
+        for x in range(self.m):
+            for y in range(self.n):
+                val = grid[x][y]
+                if val != "1":
+                    continue
+                self.num_islands += 1
+                self.dfs(x, y, grid)
+        return self.num_islands
+
+    def dfs(self, x: int, y: int, grid: List[List[str]]):
+        grid[x][y] = "0"
+        for dx, dy in DIRECTIONS:
+            nx = x + dx
+            ny = y + dy
+            if (nx < 0) or (nx >= self.m) or (ny < 0) or (ny >= self.n):
+                continue
+            val = grid[nx][ny]
+            if val != "1":
+                continue
+            self.dfs(nx, ny, grid)
+
+
+class SolutionDiscussion:
+    """
+    ##  DFS from discussion
+
+    """
+
+    def numIslands(self, grid):
+        if not grid:
+            return 0
+
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == "1":
+                    self.dfs(grid, i, j)
+                    count += 1
+        return count
+
+    def dfs(self, grid, i, j):
+        if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] != "1":
+            return
+        grid[i][j] = "#"
+        self.dfs(grid, i + 1, j)
+        self.dfs(grid, i - 1, j)
+        self.dfs(grid, i, j + 1)
+        self.dfs(grid, i, j - 1)
