2b65cf439d3a34cc505756e08cb10ae41b636c2b
mg <xynta94@gmail.com>
2022-12-26 13:52:16 +0500

    small mistake

--- leetcode/queue/133.med.CloneGraph.py
+++ leetcode/queue/133.med.CloneGraph.py
@@ -8,7 +8,7 @@ class Node:
     def __init__(self, val=0, neighbors=None):
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
-s
+
 
 class Solution:
     """
